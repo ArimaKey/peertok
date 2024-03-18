4a612600d7bd17772f778c3208be05269128a65c
Arima_Key <127151745+ArimaKey@users.noreply.github.com>
2024-03-18 14:57:48 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
 function ejecutar {
-	bash ./separate-files/alacritty.sh
-	bash ./separate-files/zsh.sh
-	bash ./separate-files/rofi.sh
-	bash ./separate-files/sxhkd.sh
-	bash ./separate-files/aditional.sh
+	sudo bash ./separate-files/alacritty.sh
+	sudo bash ./separate-files/zsh.sh
+	sudo bash ./separate-files/rofi.sh
+	sudo bash ./separate-files/sxhkd.sh
+	sudo bash ./separate-files/aditional.sh
 }
 
 function salir {
