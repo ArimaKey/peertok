b912b004770b6eb41a24e1e7fd2d8337534c808f
Arima_key <127151745+ArimaKey@users.noreply.github.com>
2024-03-18 20:18:09 -0600

    error solucionado

--- separate-files/sxhkd.sh
+++ separate-files/sxhkd.sh
@@ -34,10 +34,10 @@ chmod +x $HOME/.config/polybar/launch.sh
 sudo ./separate-files/installer.sh "feh"
 
 # Creamos una carpeta para los fondos de pantalla
-mkdir $root_file//wallpapers/
+mkdir $root_file/wallpapers/
 
 # Movemos el fondo a la carpeta destino
-cp ./resources/wallpaper.jpg $root_file//wallpapers/
+cp ./resources/wallpaper.jpg $root_file/wallpapers/
 
 # Instalando dependecias adicionales
 sudo ./separate-files/installer.sh "libconfig-dev libdbus-1-dev libegl-dev libev-dev libgl-dev libepoxy-dev libpcre2-dev libpixman-1-dev libx11-xcb-dev libxcb1-dev libxcb-composite0-dev libxcb-damage0-dev libxcb-dpms0-dev libxcb-glx0-dev libxcb-image0-dev libxcb-present-dev libxcb-randr0-dev libxcb-render0-dev libxcb-render-util0-dev libxcb-shape0-dev libxcb-util-dev libxcb-xfixes0-dev libxext-dev meson ninja-build uthash-dev"
