f317ed447ec111e157a7f276e3f990623b0a8360
Arima_Key <127151745+ArimaKey@users.noreply.github.com>
2024-03-20 09:46:57 -0600

    Rofi configuration error

--- separate-files/rofi.sh
+++ separate-files/rofi.sh
@@ -4,7 +4,8 @@ root_file="$HOME/.inst"
 sudo ./separate-files/installer.sh "rofi"
 
 # Copiamos la configuración a la carpeta .config
-cp -p ./.config/rofi/dark.rasi $HOME/.config/rofi/config.rasi
+mkdir -p $HOME/.config/rofi
+cp -p -r ./.config/rofi/dark.rasi $HOME/.config/rofi/config.rasi
 
 # Lo asignamos como archivo de configuración
 rofi -dump-theme > $HOME/.config/rofi/config.rasi
