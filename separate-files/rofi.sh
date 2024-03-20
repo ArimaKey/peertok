2f55870d1f7223ac00bad9448a8ff822df03f626
Arima_Key <127151745+ArimaKey@users.noreply.github.com>
2024-03-20 00:51:22 -0600

    Update rofi.sh

--- separate-files/rofi.sh
+++ separate-files/rofi.sh
@@ -4,7 +4,7 @@ root_file="$HOME/.inst"
 sudo ./separate-files/installer.sh "rofi"
 
 # Copiamos la configuración a la carpeta .config
-cp -r ./.config/rofi/dark.rasi $HOME/.config/config.rasi
+cp .config/rofi/dark.rasi $HOME/.config/rofi/config.rasi
 
 # Lo asignamos como archivo de configuración
 rofi -dump-theme > $HOME/.config/rofi/config.rasi
