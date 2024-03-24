5c371fa2141c97660e6f791f18b3367cd1c236b4
Arima_Key <127151745+ArimaKey@users.noreply.github.com>
2024-03-24 12:51:27 -0500

    Update rofi.sh

--- separate-files/apt/rofi.sh
+++ separate-files/apt/rofi.sh
@@ -5,6 +5,8 @@ sudo ./separate-files/apt/installer.sh "rofi"
 
 # Copiamos la configuración a la carpeta .config
 mkdir -p $HOME/.config/rofi > /dev/null 2>&1
+rm -f $HOME/.config/rofi/config > /dev/null 2>&1
+rm -f $HOME/.config/rofi/config.rasi > /dev/null 2>&1
 cp -p -r ./.config/rofi/dark.rasi $HOME/.config/rofi/config.rasi > /dev/null 2>&1
 
 # Lo asignamos como archivo de configuración
