188251983cc6b09cf7823003bb3cf66ee45857cd
Arima_Key <127151745+ArimaKey@users.noreply.github.com>
2024-03-24 12:54:08 -0500

    Update rofi.sh

--- separate-files/apt/rofi.sh
+++ separate-files/apt/rofi.sh
@@ -8,6 +8,3 @@ mkdir -p $HOME/.config/rofi > /dev/null 2>&1
 rm -f $HOME/.config/rofi/config > /dev/null 2>&1
 rm -f $HOME/.config/rofi/config.rasi > /dev/null 2>&1
 cp -p -r ./.config/rofi/dark.rasi $HOME/.config/rofi/config.rasi > /dev/null 2>&1
-
-# Lo asignamos como archivo de configuración
-rofi -dump-theme > $HOME/.config/rofi/config.rasi > /dev/null 2>&1
